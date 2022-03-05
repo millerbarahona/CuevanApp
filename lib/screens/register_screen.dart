d92ed413193a9edc4e6829febe2d9aca68982770
Miller Barahona <mbarahona@robinfood.com>
2022-03-05 17:39:16 -0500

    register form added

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -2,40 +2,69 @@ import 'package:cuevan_app/widgets/input_fields.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
-class RegisterScreen extends StatelessWidget {
+class RegisterScreen extends StatefulWidget {
    
   const RegisterScreen({Key? key}) : super(key: key);
-  
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Register'),
-        elevation: 0,
-      ),
-      body: SingleChildScrollView(
-          child: Container(
-            padding: const EdgeInsets.only(left: 10, right: 10),
-            child: Column(
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(top: 5, bottom: 10),
-                  height: 100,
-                  child: Center(child: Lottie.asset('assets/animations/register.json'))
-                ),
-                const _RegisterForm()
-              ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+            child: Container(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(top: 20, bottom: 10),
+                    alignment: Alignment.centerLeft,
+                    child: GestureDetector(
+                      child: const Icon(Icons.arrow_back_rounded, size: 45),
+                    )
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 25, bottom: 10),
+                    height: 100,
+                    child: Center(child: Lottie.asset('assets/animations/register.json'))
+                  ),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    margin: const EdgeInsets.only(top: 10, bottom: 10),
+                    child: const Text(
+                      'Register',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30
+                      )
+                    ),
+                  ),
+                  const _RegisterForm()
+                ],
+              ),
             ),
           ),
-        ),
+      ),
     );
   }
 }
 
-class _RegisterForm extends StatelessWidget {
+class _RegisterForm extends StatefulWidget {
   const _RegisterForm({
     Key? key,
   }) : super(key: key);
+
+  @override
+  State<_RegisterForm> createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<_RegisterForm> {
+  DateTime _selectedDate = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
@@ -48,9 +77,56 @@ class _RegisterForm extends StatelessWidget {
               Flexible(child: Container( padding: const EdgeInsets.only(left: 5),child: const InputField(hintText: 'Surname', obscureText: false))),
             ],
           ),
-          Container( padding: const EdgeInsets.only(top: 10), child: const InputField(hintText: 'Email', obscureText: false))
+          Container( padding: const EdgeInsets.only(top: 10), child: const InputField(hintText: 'Email', obscureText: false)),
+          Container( padding: const EdgeInsets.only(top: 10), child: const InputField(hintText: 'Password', obscureText: true)),
+          Container(
+            margin: const EdgeInsets.only(top: 10, bottom: 10),
+            height: 50,
+            width: double.infinity,
+            child: ElevatedButton(
+              style: ButtonStyle(         
+                shape: MaterialStateProperty.all<OutlinedBorder>(RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))),
+                backgroundColor: MaterialStateProperty.all<Color>(const Color(0xff2C3E50)),
+                elevation: MaterialStateProperty.all(0)
+              ),
+              child: const Text('Date of Birth'),
+              onPressed: () => _presentDatePicker(),
+              ),
+          ),
+          const Divider(),
+          Container(
+            margin: const EdgeInsets.only(top: 10),
+            width: double.infinity,
+            height: 50,
+            child: ElevatedButton(
+              style: ButtonStyle(         
+                shape: MaterialStateProperty.all<OutlinedBorder>(RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))),
+                backgroundColor: MaterialStateProperty.all<Color>(const Color(0xffa239f1)),
+                elevation: MaterialStateProperty.all(0)
+              ),
+              child: const Text('Register'),
+              onPressed: () => _presentDatePicker(),
+              ),
+          )
         ],
       ),
     );
+  }
+
+  void _presentDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(
+          DateTime.now().year, DateTime.now().month, DateTime.now().day - 6),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    });
   }
 }
