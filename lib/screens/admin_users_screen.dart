1a77ab371d967b5adef8871e8e8546ee0c6d5315
Miller Barahona <millermiller01@hotmail.com>
2022-03-10 14:40:17 -0500

    getUsers

--- lib/screens/admin_users_screen.dart
+++ lib/screens/admin_users_screen.dart
@@ -1,15 +1,35 @@
+import 'dart:convert';
+
+import 'package:cuevan_app/models/list_users_model.dart';
+import 'package:cuevan_app/models/user_model.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:http/http.dart' as http;
 
-class AdminUsersScreen extends StatelessWidget {
+class AdminUsersScreen extends StatefulWidget {
   const AdminUsersScreen({Key? key}) : super(key: key);
 
+  @override
+  State<AdminUsersScreen> createState() => _AdminUsersScreenState();
+}
+
+class _AdminUsersScreenState extends State<AdminUsersScreen> {
+
+  List<User> listOfUsers = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {    
+    super.initState();
+    _getUsersFromDB();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Container(
-          padding: const EdgeInsets.only(bottom: 10, left: 10, right: 10),
+          padding: const EdgeInsets.only( left: 10, right: 10),
           decoration: const BoxDecoration(color: Color(0xffF0F0F0)),
           child: Column(
             children: [
@@ -71,7 +91,8 @@ class AdminUsersScreen extends StatelessWidget {
                       borderRadius: BorderRadius.only(
                           topLeft: Radius.circular(20),
                           topRight: Radius.circular(20))),
-                  child: Text('listado de usurios'),
+                  child: isLoading ? _isLoadign() : _listUsersView()
+
                 ),
               )
             ],
@@ -79,5 +100,71 @@ class AdminUsersScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Widget _isLoadign() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: const [
+        CircularProgressIndicator(),
+        SizedBox(height: 20,),
+        Text('Cargando...')
+      ],
+    );
+  }
+
+  Widget _listUsersView() {
+    return ListView.builder(
+      physics: const BouncingScrollPhysics(),
+      itemCount: listOfUsers.length,
+      itemBuilder: (BuildContext context, int index) {
+        return _listUserViewItem(index);
+      },
+    );
+  }
+
+  Widget _listUserViewItem (int index) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              const Icon(Icons.account_circle_rounded),
+              Container(child: Text(listOfUsers[index].nombre), margin: const EdgeInsets.only(left: 10),)
+            ],
+          ),
+          Container(
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: const Color(0xffF0F0F0)
+            ),
+            child: Row(
+              children: const [
+                Icon(Icons.edit),
+                Icon(Icons.delete),              
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  _getUsersFromDB () async {
+    const String baseUrl ='192.168.0.8:4000/';
+    final dio = Dio();
+    //final url = Uri.http( baseUrl, 'getusers', {});
+
+    final response = await dio.get('http://192.168.0.8:4000/getusers');
+    final responseJson = json.encode(response.data);
+    final listUsers = ListUsers.fromJson(responseJson);
+
+    listOfUsers = listUsers.users;
+    isLoading = false;
+    setState(() {});
+
   }
 }
