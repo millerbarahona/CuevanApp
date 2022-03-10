61c74c6fa1bb37543600156f9d29d5f24b1ccb9f
Miller Barahona <millermiller01@hotmail.com>
2022-03-10 18:10:22 -0500

    delete user

--- lib/screens/admin_users_screen.dart
+++ lib/screens/admin_users_screen.dart
@@ -4,6 +4,7 @@ import 'package:cuevan_app/models/list_users_model.dart';
 import 'package:cuevan_app/models/user_model.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:lottie/lottie.dart';
 import 'package:http/http.dart' as http;
 
@@ -15,21 +16,21 @@ class AdminUsersScreen extends StatefulWidget {
 }
 
 class _AdminUsersScreenState extends State<AdminUsersScreen> {
-
   List<User> listOfUsers = [];
   bool isLoading = true;
 
   @override
-  void initState() {    
+  void initState() {
     super.initState();
     _getUsersFromDB();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Container(
-          padding: const EdgeInsets.only( left: 10, right: 10),
+          padding: const EdgeInsets.only(left: 10, right: 10),
           decoration: const BoxDecoration(color: Color(0xffF0F0F0)),
           child: Column(
             children: [
@@ -62,16 +63,18 @@ class _AdminUsersScreenState extends State<AdminUsersScreen> {
                               ),
                             )
                           ],
-                    )),
+                        )),
                     GestureDetector(
-                      child: const Icon(Icons.add_circle_rounded, size: 40,),
-                      onTap: () => showDialog<String>(
-                        context: context,
-                        builder: (_) => AlertDialog(
-                          title: Text('Add a new admin'),
-                        )
+                      child: const Icon(
+                        Icons.add_circle_rounded,
+                        size: 40,
                       ),
-                    )  
+                      onTap: () => showDialog<String>(
+                          context: context,
+                          builder: (_) => AlertDialog(
+                                title: Text('Add a new admin'),
+                              )),
+                    )
                   ],
                 ),
               ),
@@ -84,16 +87,14 @@ class _AdminUsersScreenState extends State<AdminUsersScreen> {
               ),
               Expanded(
                 child: Container(
-                  width: double.infinity,
-                  padding: const EdgeInsets.only(top: 20),
-                  decoration: const BoxDecoration(
-                      color: Color(0xffffffff),
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(20),
-                          topRight: Radius.circular(20))),
-                  child: isLoading ? _isLoadign() : _listUsersView()
-
-                ),
+                    width: double.infinity,
+                    padding: const EdgeInsets.only(top: 20),
+                    decoration: const BoxDecoration(
+                        color: Color(0xffffffff),
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(20),
+                            topRight: Radius.circular(20))),
+                    child: isLoading ? _isLoadign() : _listUsersView()),
               )
             ],
           ),
@@ -107,7 +108,9 @@ class _AdminUsersScreenState extends State<AdminUsersScreen> {
       mainAxisAlignment: MainAxisAlignment.center,
       children: const [
         CircularProgressIndicator(),
-        SizedBox(height: 20,),
+        SizedBox(
+          height: 20,
+        ),
         Text('Cargando...')
       ],
     );
@@ -123,7 +126,7 @@ class _AdminUsersScreenState extends State<AdminUsersScreen> {
     );
   }
 
-  Widget _listUserViewItem (int index) {
+  Widget _listUserViewItem(int index) {
     return Container(
       margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
       child: Row(
@@ -131,20 +134,37 @@ class _AdminUsersScreenState extends State<AdminUsersScreen> {
         children: [
           Row(
             children: [
-              const Icon(Icons.account_circle_rounded),
-              Container(child: Text(listOfUsers[index].nombre), margin: const EdgeInsets.only(left: 10),)
+              const Icon(
+                Icons.account_circle_rounded,
+              ),
+              Container(
+                child: Text(
+                  listOfUsers[index].nombre,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                margin: const EdgeInsets.only(left: 10),
+              )
             ],
           ),
           Container(
             padding: const EdgeInsets.all(10),
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: const Color(0xffF0F0F0)
-            ),
+                borderRadius: BorderRadius.circular(20),
+                color: const Color(0xffF0F0F0)),
             child: Row(
-              children: const [
-                Icon(Icons.edit),
-                Icon(Icons.delete),              
+              children: [
+                const Icon(Icons.edit),
+                const SizedBox(
+                  width: 5,
+                ),
+                GestureDetector(
+                  child: const Icon(
+                    Icons.delete,
+                    color: Colors.red,
+                  ),
+                  onTap: () => _showDeleteDiaglog(listOfUsers[index]),
+                ),
               ],
             ),
           )
@@ -153,8 +173,33 @@ class _AdminUsersScreenState extends State<AdminUsersScreen> {
     );
   }
 
-  _getUsersFromDB () async {
-    const String baseUrl ='192.168.0.8:4000/';
+  _showDeleteDiaglog(User user) {
+    showDialog<String>(
+        context: context,
+        builder: (_) => AlertDialog(
+          title: Text('Delete ${user.nombre}'),
+          content: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: const Color(0xffF0F0F0),
+            ),
+            width: double.infinity,
+            padding: const EdgeInsets.all(8),
+            child: const Text('Estas seguro de que quieres eliminar a este usuario?', textAlign: TextAlign.justify,),
+          ),
+          actions: [
+            TextButton(child: const Text('Aceptar', style: TextStyle(color: Color(0xffA2A2A2) ),),
+            onPressed: () => _postDeleteUserFromDB(user)),
+            TextButton(child: const Text('Cancelar'),
+            onPressed: () => {
+              Navigator.pop(context)
+            }),
+          ],
+        ));
+  }
+
+  _getUsersFromDB() async {
+    const String baseUrl = '192.168.0.8:4000/';
     final dio = Dio();
     //final url = Uri.http( baseUrl, 'getusers', {});
 
@@ -165,6 +210,21 @@ class _AdminUsersScreenState extends State<AdminUsersScreen> {
     listOfUsers = listUsers.users;
     isLoading = false;
     setState(() {});
+  }
 
+  _postDeleteUserFromDB( User user ) async {
+    final dio = Dio();
+
+    final response = await dio.delete(
+      'http://192.168.0.8:4000/deleteuser',
+      data: {"id": user.id},      
+    );
+    final responseJson = json.encode(response.data);
+    final listUsers = ListUsers.fromJson(responseJson);
+
+    listOfUsers = listUsers.users;
+    isLoading = false;
+    setState(() {});
+    Navigator.pop(context);
   }
 }
