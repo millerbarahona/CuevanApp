a6c0817bb738ac337a2ad21d4daccedea03ccbac
Julian Sarmiento <juliansarmiento10@outlook.com>
2022-03-18 00:36:12 -0500

    Validacion de campos y toast

--- lib/widgets/input_fields.dart
+++ lib/widgets/input_fields.dart
@@ -32,41 +32,49 @@ class InputField extends StatelessWidget {
       obscureText: obscureText,
       initialValue: initialValue,
       decoration: InputDecoration(
-          label: labelText != null
-              ? Container(
-                  padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 18),
-                  decoration:
-                    BoxDecoration(borderRadius: BorderRadius.circular(20),
-                    color: const Color.fromRGBO(244, 239, 248, 1),
-                    ),                                    
-                  child: Text(labelText!, style: const TextStyle(color: Color(0xffa239f1) ),))
-              : null,
-          hintText: hintText,
-          filled: true,
-          hintStyle: const TextStyle(
-            color: Color.fromRGBO(150, 151, 157, 1),
+        label: labelText != null
+            ? Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 4, horizontal: 18),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: const Color.fromRGBO(244, 239, 248, 1),
+                ),
+                child: Text(
+                  labelText!,
+                  style: TextStyle(color: Theme.of(context).primaryColor),
+                ))
+            : null,
+        hintText: hintText,
+        filled: true,
+        hintStyle: const TextStyle(
+          color: Color.fromRGBO(150, 151, 157, 1),
+        ),
+        fillColor: const Color.fromRGBO(244, 239, 248, 1),
+        prefixIcon: sufixIcon != null
+            ? Icon(
+                sufixIcon,
+                color: Theme.of(context).primaryColor,
+              )
+            : null,
+        suffixIcon: obscureText
+            ? const Icon(
+                Icons.remove_red_eye,
+                color: Color(0xffb0b0b4),
+              )
+            : null,
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15),
+          borderSide: const BorderSide(
+            color: Color.fromRGBO(245, 244, 247, 1),
           ),
-          fillColor: const Color.fromRGBO(244, 239, 248, 1),
-          prefixIcon: sufixIcon != null
-              ? Icon(
-                  sufixIcon,
-                  color: const Color(0xffa239f1),
-                )
-              : null,
-          suffixIcon: obscureText
-              ? const Icon(
-                  Icons.remove_red_eye,
-                  color: Color(0xffb0b0b4),
-                )
-              : null,
-          enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(15),
-              borderSide:
-                  const BorderSide(color: Color.fromRGBO(245, 244, 247, 1))),
-          focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(15),
-              borderSide:
-                  const BorderSide(color: Color(0xffa239f1), width: 2))),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15),
+          borderSide:
+              BorderSide(color: Theme.of(context).primaryColor, width: 2),
+        ),
+      ),
     );
   }
 }
